4961341aa8ffb2526195cda12d0d1daf66bcc2c5
APilarca <angelpill15@gmail.com>
2016-12-19 22:24:27 -0800

    create helpers for github api

--- app/containers/ConfirmBattleContainer.js
+++ app/containers/ConfirmBattleContainer.js
@@ -1,32 +1,29 @@
+
 var React = require('react');
 var ConfirmBattle = require('../components/ConfirmBattle');
+var githubHelpers = require('../utils/githubHelpers');
 
 var ConfirmBattleContainer = React.createClass({
   contextTypes: {
     router: React.PropTypes.object.isRequired
   },
   getInitialState: function () {
-    console.log('getInitialState')
     return {
       isLoading: true,
       playersInfo: [],
     }
   },
-  componentWillMount: function() {
-    console.log('componentWillMount')
-  },
   componentDidMount: function () {
     var query = this.props.location.query;
-    console.log('componentDidMount')
-    // Fetch info from github then update state
+    githubHelpers.getPlayersInfo([query.playerOne, query.playerTwo])
+      .then(function (players) {
+        this.setState({
+          isLoading: false,
+          playersInfo: [players[0], players[1]]
+        })
+      }.bind(this))
   },
-  componentWillReceiveProps: function() {
-    console.log('componentWillReceiveProps')
-  },
-  componentWillUnmount: function(){
-    console.log('componentWillUnmount')
-  },
-    render: function () {
+  render: function () {
     return (
       <ConfirmBattle
         isLoading={this.state.isLoading}
