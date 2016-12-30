f9970ff8401b34331e5d9a339832d03af19c0889
APilarca <angelpill15@gmail.com>
2016-12-30 11:45:54 -0800

    add to results page

--- app/components/Home.js
+++ app/components/Home.js
@@ -2,17 +2,18 @@ var React = require('react');
 var ReactRouter = require('react-router');
 var Link = ReactRouter.Link
 var transparentBg = require('../styles').transparentBg;
+var MainContainer = require('./MainContainer');
 
 function Home () {
-	return (
-		<div className="jumbotron cols-sm-12 text-center" style={transparentBg}>
-			<h1>Github Battle</h1>
-			<p className='lead'>What even is a jQuery?</p>
-			<Link to='/playerOne'>
-				<button type='button' className='btn btn-lg btn-success'>Get Started</button>
-			</Link>
-		</div>
-	)
+  return (
+    <MainContainer>
+      <h1>Github Battle</h1>
+      <p className='lead'>What even is a jQuery?</p>
+      <Link to='/playerOne'>
+        <button type='button' className='btn btn-lg btn-success'>Get Started</button>
+      </Link>
+    </MainContainer>
+  )
 }
 
 module.exports = Home;
